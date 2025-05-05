eed86a7930d5860dd4512da180c309bb36ea54ae
ethan-liberfit <ethan.rucar@liber.fit>
2025-05-05 15:09:51 +0200

    fix privacy policy url

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('projects/', include('projects.urls')),
     path('career', include('career.urls')),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
-    path("track", include("track.urls"))
+    path("track/", include("track.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
 
 if 'rosetta' in settings.INSTALLED_APPS:
